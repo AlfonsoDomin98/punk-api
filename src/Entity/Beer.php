5c97123b743e3e29c5b70ef9b0494fd984b1720c
alfonsodominguez <alfonsodominguezortega@gmail.com>
2022-05-05 19:54:05 +0200

    Creado la clase Beer con sus atributos y añadido el bundle maker

---
+++ src/Entity/Beer.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BeerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BeerRepository::class)
+ */
+class Beer
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var string
+     */
+    private $image;
+
+    /**
+     * @var string
+     */
+    private $tagLine;
+
+    /**
+     * @var string
+     */
+    private $firstBrewed;
+
+    /**
+     * @return int
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImage(): string
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param string $image
+     */
+    public function setImage(string $image): void
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTagLine(): string
+    {
+        return $this->tagLine;
+    }
+
+    /**
+     * @param string $tagLine
+     */
+    public function setTagLine(string $tagLine): void
+    {
+        $this->tagLine = $tagLine;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstBrewed(): string
+    {
+        return $this->firstBrewed;
+    }
+
+    /**
+     * @param string $firstBrewed
+     */
+    public function setFirstBrewed(string $firstBrewed): void
+    {
+        $this->firstBrewed = $firstBrewed;
+    }
+
+
+
+}
